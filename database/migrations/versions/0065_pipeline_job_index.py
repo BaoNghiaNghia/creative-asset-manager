d57fd51b353a357cd050c1701382f1ee9830aa98
root <baonghia.kht@gmail.com>
2026-09-09 23:01:29 +0700

    fix(migrations): shorten pipeline index revision

--- database/migrations/versions/0065_pipeline_job_index.py
+++ database/migrations/versions/0065_pipeline_job_index.py
@@ -1,13 +1,13 @@
 """add pipeline operations latest-job index
 
-Revision ID: 0065_pipeline_operations_latest_job_index
+Revision ID: 0065_pipeline_job_index
 Revises: 0064_dynamic_gemini_backups
 """
 
 from alembic import op
 
 
-revision = "0065_pipeline_operations_latest_job_index"
+revision = "0065_pipeline_job_index"
 down_revision = "0064_dynamic_gemini_backups"
 branch_labels = None
 depends_on = None
